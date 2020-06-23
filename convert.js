0ea9e96079f64866e36e122be294a7e9289781c4
Pedro Espíndula <joaopedro.jpse@gmail.com>
2020-06-23 13:30:00 -0300

    Add JSON and arg parsing to script

--- convert.js
+++ convert.js
@@ -1,11 +1,9 @@
 const toOpenApi = require('json-schema-to-openapi-schema');
 const toJsonSchema = require('to-json-schema');
 
-const obj = {
-  name: 'pedro'
-};
+const obj = JSON.parse(process.argv[process.argv.length - 1]);
 
 const schema = toJsonSchema(obj);
 const convertedSchema = toOpenApi(schema);
 
-console.log(convertedSchema)
+console.log(convertedSchema);
